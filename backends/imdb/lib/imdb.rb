147ffa3f8573980f555e6aa0cd412fef09fb0391
Jakub Kuźma <qoobaa@gmail.com>
2008-12-06 19:11:03 +0100

    Refactored IMDB backend [#7], added result type, excluded video games from movies

--- backends/imdb/lib/imdb.rb
+++ backends/imdb/lib/imdb.rb
@@ -6,39 +6,47 @@ class IMDB
   HEADERS = { "User-Agent" => "IMRb/0.0" }
 
   def movies(options = {})
+    options.merge!(:only => :movie)
+    power_search(options)
+  end
+
+  def power_search(options)
     uri = BASE_URI + "/List?"
-    uri << URI.encode(parse_list_options(options).join("&&"))
-    html = Hpricot(open(uri, HEADERS))
-    movies_html = html / "ol" / "li"
-    results = movies_html.inject([]) do |movies, movie_html|
-      movie = {}
-      # parse uri
-      uri = movie_html.at("a")["href"]
-      movie[:uri] = "#{BASE_URI}#{uri}"
-
-      # parse title
-      title = movie_html.at("a").inner_html
-      movie[:title] = sanitize_title(title)
-
-      # parse year
-      if match = title.match(/\((\d{4})\)/)
-        movie[:year] = match[1]
-      end
-
-      if match = movie_html.at("small").inner_html.match(/(\d\.\d)\/10.*\((\d+).*votes\)/)
-        movie[:rating] = match[1]
-        movie[:votes] = match[2]
-      end
-
-      movies << movie
+    uri << URI.encode(parse_power_search_options(options).join("&&"))
+    allowed_types = allowed_types(options)
+    doc = Hpricot(open(uri, HEADERS))
+    list_items = doc / "ol" / "li"
+    results = []
+    list_items.each do |list_item|
+      type = parse_type(list_item)
+      next unless allowed_types.include? type
+      result = {}
+      result[:type] = type
+      result[:title] = parse_title(list_item)
+      result[:year] = parse_year(list_item)
+      result[:type] = parse_type(list_item)
+      result[:rating] = parse_rating(list_item)
+      result[:votes] = parse_votes(list_item)
+      result[:imdb_id] = parse_imdb_id(list_item)
+      result[:uri] = parse_uri(list_item)
+      result.delete_if { |key, value| value.nil? } # compact
+      results << result
     end
     { :results => results, :query => options }
   end
 
   protected
 
-  def parse_list_options(options)
-    params = ["tvm=off", "vid=off", "tv=off", "ep=off"]
+  def parse_power_search_options(options)
+    params = []
+
+    allowed_types = allowed_types(options)
+
+    # parse types
+    params << "tvm=" + (allowed_types.include?(:tv_movie) ? "on" : "off")
+    params << "vid=" + (allowed_types.include?(:video) ? "on" : "off")
+    params << "tv=" + (allowed_types.include?(:tv_serie) ? "on" : "off")
+    params << "ep=" +  (allowed_types.include?(:tv_episode) ? "on" : "off")
 
     # parse year option
     case options[:year]
@@ -64,13 +72,71 @@ class IMDB
     # parse votes option
     params << "votes=#{options[:votes]}" if options[:votes]
 
-    # parse keywords option
-    params << "words=#{options[:keywords]}" if options[:keywords]
+    # parse title keywords option
+    params << "words=#{options[:title_keywords]}" if options[:title_keywords]
 
     params
   end
 
-  def sanitize_title(title)
-    title.gsub(/\(\d{4}\)/, "").strip.squeeze(" ")
+  def allowed_types(options)
+    types = [:movie, :tv_episode, :tv_movie, :tv_serie, :video, :video_game]
+    if options.has_key? :only and options.has_key? :except
+      raise ArgumentError, "using :except and :only at the same time is not allowed"
+    elsif options.has_key? :only
+      types &= [options[:only]].flatten
+    elsif options.has_key? :except
+      types -= [options[:except]].flatten
+    end
+    types
+  end
+
+  def parse_title(list_item)
+    a_html = list_item.at("a").inner_html
+    a_html.gsub(/\(\d{4}(\/[^)]+)?\)|\(TV\)|\(V\)|\(VG\)/, "").strip.squeeze(" ")
+  end
+
+  def parse_uri(list_item)
+    a_href = list_item.at("a")["href"]
+    "#{BASE_URI}#{a_href}"
+  end
+
+  def parse_imdb_id(list_item)
+    a_href = list_item.at("a")["href"]
+    match = a_href.match(/\/title\/tt(\d+)/)
+    match[1] if match
+  end
+
+  def parse_year(list_item)
+    a_html = list_item.at("a").inner_html
+    match = a_html.match(/\((\d{4})(\/[^)]+)?\)/)
+    match[1] if match
+  end
+
+  def parse_rating(list_item)
+    (list_item / "small").each do |small|
+      match = small.inner_html.match(/(\d{1,2}.\d)\/10.*\(\d+.*votes\)/)
+      return match[1].to_f if match
+    end
+    nil
+  end
+
+  def parse_votes(list_item)
+    (list_item / "small").each do |small|
+      match = small.inner_html.match(/\d\.\d\/10.*\((\d+).*votes\)/)
+      return match[1].to_i if match
+    end
+    nil
+  end
+
+  def parse_type(list_item)
+    a_html = list_item.at("a").inner_html
+    return :tv_movie if a_html.include? "(TV)"
+    return :video_game if a_html.include? "(VG)"
+    return :video if a_html.include? "(V)"
+    (list_item / "small").each do |small|
+      return :tv_serie if small.inner_html.include? "TV Series"
+      return :tv_episode if small.inner_html.include? "TV Episode"
+    end
+    :movie
   end
 end
--- backends/imdb/spec/lib/imdb_spec.rb
+++ backends/imdb/spec/lib/imdb_spec.rb
@@ -1,49 +1,49 @@
 require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
-describe IMDB, "#parse_list_options" do
+describe IMDB, "#parse_power_search_options" do
   before do
-    IMDB.send(:public, :parse_list_options)
+    IMDB.send(:public, :parse_power_search_options)
     @imdb = IMDB.new
   end
 
   it "should contain default params" do
-    params = @imdb.parse_list_options({})
-    params.should include("tvm=off")
-    params.should include("vid=off")
-    params.should include("tv=off")
-    params.should include("ep=off")
+    params = @imdb.parse_power_search_options({})
+    params.should include("tvm=on")
+    params.should include("vid=on")
+    params.should include("tv=on")
+    params.should include("ep=on")
   end
 
   it "should parse year option" do
-    params = @imdb.parse_list_options(:year => 2008)
+    params = @imdb.parse_power_search_options(:year => 2008)
     params.should include("year=2008")
   end
 
   it "should parse year range option" do
-    params = @imdb.parse_list_options(:year => 2000..2005)
+    params = @imdb.parse_power_search_options(:year => 2000..2005)
     params.should include("year_lo=2000")
     params.should include("year_hi=2005")
   end
 
   it "should parse rating option" do
-    params = @imdb.parse_list_options(:rating => 6)
+    params = @imdb.parse_power_search_options(:rating => 6)
     params.should include("lo-rating=6")
     params.should include("hi-rating=10")
   end
 
   it "should parse rating range option" do
-    params = @imdb.parse_list_options(:rating => 1..5)
+    params = @imdb.parse_power_search_options(:rating => 1..5)
     params.should include("lo-rating=1")
     params.should include("hi-rating=5")
   end
 
   it "should parse votes option" do
-    params = @imdb.parse_list_options(:votes => 1000)
+    params = @imdb.parse_power_search_options(:votes => 1000)
     params.should include("votes=1000")
   end
 
   it "should parse keywords option" do
-    params = @imdb.parse_list_options(:keywords => "a b c")
+    params = @imdb.parse_power_search_options(:title_keywords => "a b c")
     params.should include("words=a b c")
   end
 end
@@ -58,16 +58,18 @@ describe IMDB, "using babylon.html fixture" do
 
   it "should parse HTML and return hash" do
     movies = @imdb.movies
-    movies.should include(:query => {})
+    movies.should have_key(:query)
     movies.should have_key(:results)
     results = movies[:results]
     results.count.should == 1
     result = results.first
     result.should include(:title => "Babylon A.D.")
     result.should include(:year => "2008")
-    result.should include(:rating => "5.3")
-    result.should include(:votes => "9678")
+    result.should include(:rating => 5.3)
+    result.should include(:votes => 9678)
     result.should include(:uri => "http://www.imdb.com/title/tt0364970/")
+    result.should include(:type => :movie)
+    result.should include(:imdb_id => "0364970")
   end
 end
 
@@ -80,7 +82,7 @@ describe IMDB, "using notfound.html fixture" do
 
   it "should parse HTML and return hash" do
     movies = @imdb.movies
-    movies.should include(:query => {})
+    movies.should have_key(:query)
     movies.should have_key(:results)
     movies[:results].should be_empty
   end
@@ -93,42 +95,42 @@ describe IMDB, "using monkeys.html fixture" do
   end
 
   it "should parse HTML and return hash" do
-    movies = @imdb.movies
-    movies.should include(:query => {})
+    movies = @imdb.power_search(:only => :tv_movie)
+    movies.should have_key(:query)
     movies.should have_key(:results)
     results = movies[:results]
     results.count.should == 5
 
     result = results[0]
-    result.should include(:title => "Dana Carvey: Squatting Monkeys Tell No Lies (TV)")
+    result.should include(:title => "Dana Carvey: Squatting Monkeys Tell No Lies")
     result.should include(:year => "2008")
-    result.should include(:rating => "6.7")
-    result.should include(:votes => "55")
+    result.should include(:rating => 6.7)
+    result.should include(:votes => 55)
     result.should include(:uri => "http://www.imdb.com/title/tt1251752/")
 
     result = results[1]
-    result.should include(:title => "Monkey, Monkey, Bottle of Beer, How Many Monkeys Have We Here? (TV)")
+    result.should include(:title => "Monkey, Monkey, Bottle of Beer, How Many Monkeys Have We Here?")
     result.should include(:year => "1974")
     result.should_not have_key(:rating)
     result.should_not have_key(:votes)
     result.should include(:uri => "http://www.imdb.com/title/tt0211524/")
 
     result = results[2]
-    result.should include(:title => "Monkeys (TV)")
+    result.should include(:title => "Monkeys")
     result.should include(:year => "1989")
-    result.should include(:rating => "6.6")
-    result.should include(:votes => "5")
+    result.should include(:rating => 6.6)
+    result.should include(:votes => 5)
     result.should include(:uri => "http://www.imdb.com/title/tt0338260/")
 
     result = results[3]
-    result.should include(:title => "Snow Monkeys (TV)")
+    result.should include(:title => "Snow Monkeys")
     result.should include(:year => "1994")
     result.should_not have_key(:rating)
     result.should_not have_key(:votes)
     result.should include(:uri => "http://www.imdb.com/title/tt0220050/")
 
     result = results[4]
-    result.should include(:title => "Uncle Gus in: For the Love of Monkeys (TV)")
+    result.should include(:title => "Uncle Gus in: For the Love of Monkeys")
     result.should include(:year => "1999")
     result.should_not have_key(:rating)
     result.should_not have_key(:votes)
